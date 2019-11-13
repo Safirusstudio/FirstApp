d7fd1ffbea8614b41bc15acfd4ad927450199454
Ruslan <ruslan2507@gmail.com>
2019-11-13 12:09:15 +0600

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Руслан on 11/13/19.
+//  Copyright © 2019 Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
